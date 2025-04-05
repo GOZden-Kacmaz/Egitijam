7bb3287b3bc5f831c6c509238f7fad43aa41c77e
Muhittin Ali Akyüz <akyuzhome.ali21@gmail.com>
2025-04-05 18:53:52 +0300

    oyun

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -14,15 +15,48 @@ public class GameManager : MonoBehaviour
     public GameObject dialog;
     public GameObject dialog2;
     public GameObject tpmenu;
+    [SerializeField] private bool bulmacaa;
+    [SerializeField] private bool[] ipucu1;
+    [SerializeField] private Button[] buttons;
+    public GameManager gameManager;
+    public bool ayakalk;
 
-
-    void Start()
+    private void Start()
     {
-        
-    }
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            int index = i;
+            buttons[i].onClick.AddListener(() => OnButtonClicked(index));
 
+        }
+    }
+    public bool hepsitruemi()
+    {
+        foreach (bool deger in ipucu1)
+        {
+            if (!deger)
+                return false;
+        }
+        return true;
+    }
+    private void OnButtonClicked(int index)
+    {
+        if (index >= 0 && index < ipucu1.Length)
+        {
+            ipucu1[index] = true;
+        }
+    }
     void Update()
     {
-        
+        ayakalk = hepsitruemi();
+        if (Input.GetKeyDown(KeyCode.E)&& ayakalk)
+        {
+            kamera1.enabled = false;
+            kamera2.enabled = false;
+            kamera3.enabled = false;
+            kamera4.enabled = true;
+
+        }
+
     }
 }
