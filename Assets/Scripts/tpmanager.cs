0c94eb5febe251021ed1f53b02feb249e7e7e00a
AliEfenver <aliefeunver3519@gmail.com>
2025-04-05 07:41:49 +0300

    a

---
+++ Assets/Scripts/tpmanager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tpmanager : MonoBehaviour
+{
+    public GameManager gamemanagersc;
+    
+    void Start()
+    {
+
+    }
+
+    public void EvTP()
+    {
+
+
+        gamemanagersc.kamera1.enabled = false;
+        gamemanagersc.kamera2.enabled = false;
+        gamemanagersc.kamera3.enabled = false;
+        gamemanagersc.kamera4.enabled = true;
+        gamemanagersc.tpmenu.SetActive(false);
+        gamemanagersc.PlayerScript.gameObject.transform.position = new Vector3(32, -18, 0);
+
+
+    }
+
+
+
+    void Update()
+    {
+
+    }
+
+}
