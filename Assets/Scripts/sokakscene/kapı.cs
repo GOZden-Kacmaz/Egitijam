a6e8f1c868daa090cc46c42fda77dd0f44c10755
Muhittin Ali Akyüz <akyuzhome.ali21@gmail.com>
2025-04-06 17:32:23 +0300

    yükle

--- Assets/Scripts/sokakscene/kapı.cs
+++ Assets/Scripts/sokakscene/kapı.cs
@@ -1,97 +1,109 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
+using System.Collections;
 
 public class kapı : MonoBehaviour
 {
     public static int kapilarindex;
     public static bool kapical = false;
-    public  GameObject[] dialog;
-    [SerializeField] public Transform kamera;
+
+    public GameObject[] dialog;
+    [SerializeField] private Transform kamera;
     [SerializeField] private TMP_Text[] DialogText;
     [SerializeField] private string[] kapi1cumle;
     [SerializeField] private string[] kapi2cumle;
+    [SerializeField] private string[] kapi3cumle;
     [SerializeField] private float yazmaHızı = 0.05f;
-    private int index;
+
+    private int[] index;
     public player player;
     public KameraManager KameraScript;
+    private bool dialogComplete = false;
 
     private void Start()
     {
-        for (int i =0; i == dialog.Length; i++)
+        index = new int[dialog.Length];
+
+        for (int i = 0; i < dialog.Length; i++)
         {
             dialog[i].SetActive(false);
+            index[i] = 0;
         }
-        
     }
 
     void Update()
     {
-        // Kapıya yaklaşıldıysa ve E tuşuna basıldıysa
-        if (kapical && Input.GetKeyDown(KeyCode.E) && kapilarindex == 0)
+        if (kapical && Input.GetKeyDown(KeyCode.E) && !dialogComplete)
         {
-            KameraScript.karakterAktif = false;
-            dialog[0].SetActive(true);
-            kamera.position = new Vector3(0.8f, -11.35f, kamera.position.z);
-            player.gameObject.SetActive(false);
-            index++;
-            if (index < kapi1cumle.Length)
-            {
-                    StartCoroutine(kapi1yaz());
-            }
-            if (index >= kapi1cumle.Length)
-            {
-                dialog[0].SetActive(false);
-                kamera.position = new Vector3();
-                player.gameObject.SetActive(true);
-                KameraScript.karakterAktif = true;
-            }
-        }
-       
-        if (kapical && Input.GetKeyDown(KeyCode.E) && kapilarindex == 1)
-        {
-            KameraScript.karakterAktif = false;
-            dialog[1].SetActive(true);
-            kamera.position = new Vector3(19.74f, -11.35f, kamera.position.z);
-            player.gameObject.SetActive(false);
-            index++;
-            if (index < kapi2cumle.Length)
-            {
-                StartCoroutine(kapi2yaz());
-            }
-            if (index >= kapi2cumle.Length)
-            {
-                dialog[1].SetActive(false);
-                kamera.position = new Vector3();
-                player.gameObject.SetActive(true);
-                KameraScript.karakterAktif = true;
-            }
-        }
-       
-       
-      
-        
-    }
-    IEnumerator kapi1yaz()
-    {
-        DialogText[0].text = "";
+            KameraScript.SetKarakterActive(false);
 
-        foreach (char harf in kapi1cumle[index])
+            for (int i = 0; i < index.Length; i++)
+            {
+                index[i] = 0;
+            }
+
+            switch (kapilarindex)
+            {
+                case 0:
+                    dialog[0].SetActive(true);
+                    kamera.position = new Vector3(0.95f, -11.69f, kamera.position.z);
+                    StartCoroutine(KapiYaz(kapi1cumle, 0));
+                    break;
+                case 1:
+                    dialog[1].SetActive(true);
+                    kamera.position = new Vector3(21.67f, -11.69f, kamera.position.z);
+                    StartCoroutine(KapiYaz(kapi2cumle, 1));
+                    break;
+                case 2:
+                    dialog[2].SetActive(true);
+                    kamera.position = new Vector3(43.58f, -11.69f, kamera.position.z);
+                    StartCoroutine(KapiYaz(kapi3cumle, 2));
+                    break;
+            }
+        }
+
+        if (dialogComplete && Input.GetKeyDown(KeyCode.Space))
         {
-            DialogText[1].text += harf;
+            player.gameObject.SetActive(true);
+
+            Vector3 yeniPozisyon;
+
+            if (player.transform.position.x < 0)
+            {
+                yeniPozisyon = new Vector3(0.7f, 0.42f, -2);
+            }
+            else
+            {
+                yeniPozisyon = new Vector3(player.transform.position.x - 3f, 0.42f, -2);
+            }
+
+            KameraScript.KameraGecisPozisyonu(yeniPozisyon);
+            KameraScript.SetKarakterActive(true);
+
+            dialog[kapilarindex].SetActive(false);
+            dialogComplete = false;
+        }
+    }
+
+    IEnumerator KapiYaz(string[] cumleler, int dialogIndex)
+    {
+        DialogText[dialogIndex].text = "";
+
+        foreach (char harf in cumleler[index[dialogIndex]])
+        {
+            DialogText[dialogIndex].text += harf;
             yield return new WaitForSeconds(yazmaHızı);
         }
-    }
-    IEnumerator kapi2yaz()
-    {
-        DialogText[1].text = "";
 
-        foreach (char harf in kapi2cumle[index])
+        index[dialogIndex]++;
+
+        if (index[dialogIndex] < cumleler.Length)
         {
-            DialogText[2].text += harf;
-            yield return new WaitForSeconds(yazmaHızı);
+            StartCoroutine(KapiYaz(cumleler, dialogIndex));
+        }
+        else
+        {
+            dialogComplete = true;
         }
     }
-
 }
